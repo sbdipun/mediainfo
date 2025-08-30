0cbcbf927658627c31b6e717edd4d7dba199cac1
sbdipun <87560288+sbdipun@users.noreply.github.com>
2025-08-30 23:44:27 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update package list
-apt-get update
-
-# Install MediaInfo library
-apt-get install -y libmediainfo0v5 libmediainfo-dev mediainfo
-
-# Install Python dependencies
-pip install -r requirements.txt
